9ea8c7d20170c7a9fb3b07090bd0160e451c1fef
Danyl Tomchuk <bikokzip@gmail.com>
2025-11-16 13:04:35 +0100

    add new section simple-recipes

---
+++ scripts/ButtonLike.js
@@ -0,0 +1,31 @@
+class ButtonLike {
+  elements = {
+    buttonLike: "[data-js-button-like]",
+  }
+
+  stateClasses = {
+    like: "like",
+    dislike: "dislike"
+  }
+
+  constructor() {
+    this.buttonElements = document.querySelectorAll(this.elements.buttonLike)
+    this.bindEvent()
+  }
+
+  bindEvent() {
+    this.buttonElements.forEach(buttonElement => {
+      buttonElement.addEventListener("click", () => {
+        buttonElement.classList.toggle(this.stateClasses.like)
+  
+        if (buttonElement.classList.contains(this.stateClasses.like)) {
+          buttonElement.ariaLabel = this.stateClasses.like
+        } else {
+          buttonElement.ariaLabel = this.stateClasses.dislike
+        }
+      })
+    });
+  }
+}
+
+export default ButtonLike
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import Header from "./Header.js"
 import ScrollRecipes from "./ScrollRecipes.js"
+import ButtonLike from "./ButtonLike.js"
 
 new Header()
 new ScrollRecipes()
+new ButtonLike()
